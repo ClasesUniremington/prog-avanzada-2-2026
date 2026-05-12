3020d42646a5e3187b8b900ab72b62669a0c0fa0
JAMESOSSA <jossa6772@gmail.com>
2026-05-11 21:00:15 -0500

    examen1_james_ossa

--- Unit-Test/examen-productos/src/test/java/co/edu/uniremington/service/ProductServiceTest.java
+++ Unit-Test/examen-productos/src/test/java/co/edu/uniremington/service/ProductServiceTest.java
@@ -26,15 +26,35 @@ public class ProductServiceTest {
     @Test
     public void debeGuardarProductoCorrectamente() {
         // TODO: ESTUDIANTE: 1) Instanciar un producto con datos válidos
+        Product product = new Product(1L, "Laptop", new BigDecimal("2500"));
+
         // TODO: ESTUDIANTE: 2) Llamar al método createProduct del productService
+        productService.createProduct(product);
+
         // TODO: ESTUDIANTE: 3) Usar ArgumentCaptor para capturar el producto enviado al repositorio
+        ArgumentCaptor<Product> captor =
+                ArgumentCaptor.forClass(Product.class);
         // TODO: ESTUDIANTE: 4) Verificar con assert que los datos del producto capturado coinciden con los creados
+        verify(repository).save(captor.capture());
+        // 5. Obtener el producto capturado
+        Product captured = captor.getValue();
+
+        // 6. Validar que los datos sean iguales
+        assertEquals(product.getId(), captured.getId());
+        assertEquals(product.getName(), captured.getName());
+        assertEquals(product.getPrice(), captured.getPrice());
     }
 
     @Test
     public void debeLanzarExcepcionCuandoElPrecioEsNegativo() {
         // TODO: ESTUDIANTE: 1) Instanciar un producto con precio negativo
+        Product product = new Product(1L, "Laptop", new BigDecimal("-100")
+        );
         // TODO: ESTUDIANTE: 2) Usar assertThrows para verificar que se lanza IllegalArgumentException 
-        //                      al llamar a productService.createProduct()
+        //   al llamar a productService.createProduct()
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> productService.createProduct(product));
     }
 }
