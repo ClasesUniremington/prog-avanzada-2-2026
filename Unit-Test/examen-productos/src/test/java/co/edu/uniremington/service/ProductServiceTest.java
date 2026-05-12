5bd0c281c51f05748cc724c983a47b79eade141b
Sara Gallego <saragallego207@gmail.com>
2026-05-11 22:33:04 -0500

    feat: pruebas unitarias ProductServiceTest

--- Unit-Test/examen-productos/src/test/java/co/edu/uniremington/service/ProductServiceTest.java
+++ Unit-Test/examen-productos/src/test/java/co/edu/uniremington/service/ProductServiceTest.java
@@ -2,17 +2,19 @@ package co.edu.uniremington.service;
 
 import co.edu.uniremington.model.Product;
 import co.edu.uniremington.repository.ProductRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 public class ProductServiceTest {
@@ -26,15 +28,39 @@ public class ProductServiceTest {
     @Test
     public void debeGuardarProductoCorrectamente() {
         // TODO: ESTUDIANTE: 1) Instanciar un producto con datos válidos
+
+        Product product = new Product(1L, "Laptop", new BigDecimal("1500.00"));
+
         // TODO: ESTUDIANTE: 2) Llamar al método createProduct del productService
+
+        productService.createProduct(product);
+        Mockito.verify(repository, Mockito.times(1)).save(product);
+
+
         // TODO: ESTUDIANTE: 3) Usar ArgumentCaptor para capturar el producto enviado al repositorio
+
+        ArgumentCaptor<Product> captor = ArgumentCaptor.forClass(Product.class);
+        Mockito.verify(repository).save(captor.capture());
+        Product capturado = captor.getValue();
+
         // TODO: ESTUDIANTE: 4) Verificar con assert que los datos del producto capturado coinciden con los creados
+
+        assertEquals("Laptop", capturado.getName());
+        assertEquals(new BigDecimal("1500.00"), capturado.getPrice());
     }
 
     @Test
     public void debeLanzarExcepcionCuandoElPrecioEsNegativo() {
         // TODO: ESTUDIANTE: 1) Instanciar un producto con precio negativo
-        // TODO: ESTUDIANTE: 2) Usar assertThrows para verificar que se lanza IllegalArgumentException 
+
+        Product product = new Product(2L, "Mouse", new BigDecimal("-10.00"));
+
+        // TODO: ESTUDIANTE: 2) Usar assertThrows para verificar que se lanza IllegalArgumentException
         //                      al llamar a productService.createProduct()
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> productService.createProduct(product)
+        );
+
     }
 }
