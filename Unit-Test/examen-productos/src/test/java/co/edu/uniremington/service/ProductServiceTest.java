8e146eb3cf545d0cb71f6c0ae4fb9b135374bcac
AndresVargas5 <andres.vargas.marin@gmail.com>
2026-05-11 20:52:37 -0500

    Unitary test exam1

--- Unit-Test/examen-productos/src/test/java/co/edu/uniremington/service/ProductServiceTest.java
+++ Unit-Test/examen-productos/src/test/java/co/edu/uniremington/service/ProductServiceTest.java
@@ -25,16 +25,39 @@ public class ProductServiceTest {
 
     @Test
     public void debeGuardarProductoCorrectamente() {
+
         // TODO: ESTUDIANTE: 1) Instanciar un producto con datos válidos
+        Product producto = new Product();
+        producto.setName("Laptop HP");
+        producto.setPrice(new BigDecimal("1500.00"));
+
         // TODO: ESTUDIANTE: 2) Llamar al método createProduct del productService
+        productService.createProduct(producto);
+
         // TODO: ESTUDIANTE: 3) Usar ArgumentCaptor para capturar el producto enviado al repositorio
+        ArgumentCaptor<Product> productoCaptor = ArgumentCaptor.forClass(Product.class);
+        verify(repository).save(productoCaptor.capture());
+
         // TODO: ESTUDIANTE: 4) Verificar con assert que los datos del producto capturado coinciden con los creados
+        Product productoCaptured = productoCaptor.getValue();
+        assertEquals("Laptop HP", productoCaptured.getName());
+        assertEquals(new BigDecimal("1500.00"), productoCaptured.getPrice());
+
     }
 
     @Test
     public void debeLanzarExcepcionCuandoElPrecioEsNegativo() {
         // TODO: ESTUDIANTE: 1) Instanciar un producto con precio negativo
+        Product producto = new Product();
+        producto.setName("Producto Inválido");
+        producto.setPrice(new BigDecimal("-100.00"));
+
         // TODO: ESTUDIANTE: 2) Usar assertThrows para verificar que se lanza IllegalArgumentException 
         //                      al llamar a productService.createProduct()
+        IllegalArgumentException exception = assertThrows( //assertThrows metodo que lanza una expresion especifica
+                IllegalArgumentException.class,
+                () -> productService.createProduct(producto)//ejecuta el metodo que se quiere probar
+        );
+
     }
 }
