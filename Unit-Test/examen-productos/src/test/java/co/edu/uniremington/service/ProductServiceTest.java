53902c6aacd4fd0ade16ccc3fd1449bd1981a006
ASUS <andergongut@hotmail.com>
2026-05-25 19:09:12 -0500

    feat: avance unit test

--- Unit-Test/examen-productos/src/test/java/co/edu/uniremington/service/ProductServiceTest.java
+++ Unit-Test/examen-productos/src/test/java/co/edu/uniremington/service/ProductServiceTest.java
@@ -26,15 +26,43 @@ public class ProductServiceTest {
     @Test
     public void debeGuardarProductoCorrectamente() {
         // TODO: ESTUDIANTE: 1) Instanciar un producto con datos válidos
+        Product product = new Product;
+        product.setName("Lapicero");
+        product.setValor(new BigDecimal("3000.00"));
+        product.setStock(5);
+
         // TODO: ESTUDIANTE: 2) Llamar al método createProduct del productService
+        productService.createProduct(product);
+
         // TODO: ESTUDIANTE: 3) Usar ArgumentCaptor para capturar el producto enviado al repositorio
+        ArgumentCaptor<Product> captor = ArgumentCaptor.forClass(Product.class);
+        Mockito.verify(productRepository).save(captor.capture());
+
+        // IMPORTANTE: Obtener el producto capturado
+        Product capturedProduct = captor.getValue();
+
         // TODO: ESTUDIANTE: 4) Verificar con assert que los datos del producto capturado coinciden con los creados
+        assertEquals(product.getName(), capturedProduct.getName());
+        assertEquals(product.getValor(), capturedProduct.getValor());
+        assertEquals(product.getStock(), capturedProduct.getStock());
     }
 
     @Test
     public void debeLanzarExcepcionCuandoElPrecioEsNegativo() {
         // TODO: ESTUDIANTE: 1) Instanciar un producto con precio negativo
-        // TODO: ESTUDIANTE: 2) Usar assertThrows para verificar que se lanza IllegalArgumentException 
+        Product product = new Product();
+        product.setName("Lapicero");
+        product.setValor(new BigDecimal("-3000.00"));
+        product.setStock(5);
+        // TODO: ESTUDIANTE: 2) Usar assertThrows para verificar que se lanza IllegalArgumentException
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> productService.createProduct(product)
+        );
+
+
         //                      al llamar a productService.createProduct()
+        Mockito.verify(productRepository, Mockito.never())
+                .save(Mockito.any(Product.class));
     }
 }
